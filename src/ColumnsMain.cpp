f8e03e3b3636b5ce66c8f2d497366671e12af017
ldornbusch <ldornbusch@yogasurftech.de>
2020-08-25 18:36:56 +0200

    set Window and app Icon

--- src/ColumnsMain.cpp
+++ src/ColumnsMain.cpp
@@ -1,10 +1,12 @@
+
 #include "Columns/ColumnsGameController.h"
 
 int main() {
     ColumnsGameController ColumnsCtrl;
-   	All2D_init(640, 480, "WinterColumns by hexerei software creations (c) 2000-2003");
+    Image icon;
+    icon.loadImage("data/icon.png");
+   	All2D_init(640, 480, "WinterColumns by hexerei software creations (c) 2000-2003",&icon);
 
     int retval = All2D_main(ColumnsCtrl);
     return retval;
 }
-
